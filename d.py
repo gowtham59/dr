bc05471982398ddd322a37fd0b4ecf128c737021
gowtham59 <47033355+gowtham59@users.noreply.github.com>
2019-07-03 16:25:19 +0530

    Update d.py

--- d.py
+++ d.py
@@ -4,7 +4,7 @@ q3=[]
 for i in range(p1):
     q3=list(map(int,input().split()))
     for j in range(len(q3)):
-        l3.append(q3[j])
+        l2.append(q3[j])
 l2.sort()
 for i in range(len(l2)-1):
     print(l2[i],end=' ')
